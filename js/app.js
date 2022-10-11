e3bae759bcdc4bb4e75bb82ae405172bab422e55
COBRA <piyushghosh2021@gmail.com>
2022-10-11 16:43:49 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// const getElement = (selector) => {
+//   const element = document.querySelector(selector);
+//   if (element) {
+//     return element;
+//   }
+//   throw Error(`${selector} class not found `);
+// };
+
+// const links = getElement(".nav-links");
+// const navBtn = getElement(".nav-btn");
+// navBtn.addEventListener("click", () => {
+//   links.classList.toggle("show-links");
+// });
